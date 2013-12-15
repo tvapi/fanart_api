92323bc78a333ba0e53ccbcf320103301399b0d5
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2013-12-15 23:16:02 +0100

    fix music class

--- lib/fanart_api/music.rb
+++ lib/fanart_api/music.rb
@@ -1,12 +1,8 @@
 # documentation: http://fanart.tv/api-docs/music-api
 class FanartApi::Music < FanartApi::Base
-  def initialize(client)
-    super(client)
-
-    [:artist, :album, :label].each do |method_name|
-      define_method(method_name) do |id, type = 'all', sort = '1', limit = '2'|
-        self.class.get("#{method_name}#{shared_uri(id, type, sort, limit)}")
-      end
+  [:artist, :album, :label].each do |method_name|
+    define_method(method_name) do |id, type = 'all', sort = '1', limit = '2'|
+      self.class.get("#{method_name}#{shared_uri(id, type, sort, limit)}")
     end
   end
 
