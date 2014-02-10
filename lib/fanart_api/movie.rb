ceec84f5021c3020875903fc7492e5ddac5ac15b
Justin Nemeth <justin@trakt.tv>
2014-02-09 22:43:03 -0800

    fix movie endpoint

--- lib/fanart_api/movie.rb
+++ lib/fanart_api/movie.rb
@@ -1,7 +1,7 @@
 # documentation: http://fanart.tv/api-docs/movie-api
 class FanartApi::Movie < FanartApi::Base
   def find(id, type = 'all', sort = '1', limit = '2')
-    get("movies/#{shared_uri}").params(id: id, type: type, sort: sort, limit: limit).response
+    get("movie/#{shared_uri}").params(id: id, type: type, sort: sort, limit: limit).response
   end
 
   def update(timestamp = (Time.now - 172800).to_i)
